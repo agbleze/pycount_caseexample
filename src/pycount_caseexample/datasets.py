c4ccd00096dbf5d79976a04c69ac76c209ec78d7
linus <lagbleze@st.ug.edu.gh>
2024-10-05 17:25:28 +0200

    fix: add data

--- src/pycount_caseexample/datasets.py
+++ src/pycount_caseexample/datasets.py
@@ -19,4 +19,17 @@ def get_flatland():
     return data_file_path
 
 def get_zen():
-    pass
+    """Get path to example "Flatland" [1]_ text file.
+
+    Returns
+    -------
+    pathlib.PosixPath
+        Path to file.
+
+    References
+    ----------
+    .. [1] E. A. Abbott, "Flatland", Seeley & Co., 1884.
+    """
+    with resources.path("pycount_caseexample.data", "zen.txt") as f:
+        data_file_path = f
+    return data_file_path
